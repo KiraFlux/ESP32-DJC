77244448b71dfd58890aaf620be8b98175cadcaf
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-11-20 15:39:13 +0300

    Pong Game refactor

--- DJC-Firmware/src/djc/behaviors/PongGame.hpp
+++ DJC-Firmware/src/djc/behaviors/PongGame.hpp
@@ -6,71 +6,77 @@
 
 #include "djc/Periphery.hpp"
 
+
 namespace djc {
 
 struct PongGame : kf::sys::Behavior, kf::tools::Singleton<PongGame> {
     friend struct Singleton<PongGame>;
 
 private:
+    using Canvas = kf::gfx::Canvas;
+
     struct Wall {
+        enum class Side { Left, Right };
 
-        enum class Side {
-            Left,
-            Right,
-        };
+    private:
 
-        kf::Joystick &joy;
         kf::Pixel size{0}, range{0};
         kf::Pixel x{0}, y{0};
 
-        void update(const kf::gfx::Canvas &canvas) {
+    public:
+
+        void update(kf::f32 v) {
             y = range;
             y /= 2;
-            y -= range * (joy.axis_y.read() * 0.5);
+            y = static_cast<kf::Pixel>(
+                y - static_cast<kf::Pixel>(v * kf::f32(range) * 0.5f)
+            );
         }
 
-        void render(kf::gfx::Canvas &canvas) {
-            canvas.line(x, y, x, y + size);
+        void render(Canvas &canvas) const {
+            canvas.line(x, y, x, static_cast<kf::Pixel>(y + size));
         }
 
-        void reset(const kf::gfx::Canvas &canvas, Side side) {
-            size = canvas.height() / 4;
-            range = canvas.height() - size;
-            y = canvas.centerY();
-            x = 6;
+        void reset(const Canvas &canvas, Side side) {
+            size = static_cast<kf::Pixel>(canvas.height() / 4);
+            range = static_cast<kf::Pixel>(canvas.height() - size);
 
+            x = static_cast<kf::Pixel>(canvas.width() / 20);
             if (Side::Right == side) {
-                x = canvas.width() - x;
+                x = static_cast<kf::Pixel>(canvas.width() - x);
             }
+
+            y = canvas.centerY();
         }
 
-    } left_wall{Periphery::instance().left_joystick}, right_wall{Periphery::instance().right_joystick};
+    };
 
     struct Ball {
+    private:
         kf::Pixel radius{0};
         kf::Pixel x{0}, y{0};
         kf::Pixel speed_x{0}, speed_y{0};
-
-        void update(const kf::gfx::Canvas &canvas) {
-            x += speed_x;
-            y += speed_y;
+    public:
+        void update(const Canvas &canvas) {
+            x = static_cast<kf::Pixel>(x + speed_x);
+            y = static_cast<kf::Pixel>(y + speed_y);
 
             if (isOutOfCanvasX(canvas)) {
-                speed_x = -speed_x;
+                speed_x = static_cast<kf::Pixel>(-speed_x);
             }
 
             if (isOutOfCanvasY(canvas)) {
-                speed_y = -speed_y;
+                speed_y = static_cast<kf::Pixel>(-speed_y);
             }
         }
 
-        void render(kf::gfx::Canvas &canvas) {
+        void render(Canvas &canvas) const {
             canvas.dot(x, y);
-            canvas.circle(x, y, radius, kf::gfx::Canvas::Mode::FillBorder);
+            canvas.circle(x, y, radius, Canvas::Mode::FillBorder);
         }
 
-        void reset(const kf::gfx::Canvas &canvas) {
-            radius = std::min(canvas.width(), canvas.height()) >> 4;
+        void reset(const Canvas &canvas) {
+            radius = static_cast<kf::Pixel>(std::min(canvas.width(), canvas.height()) / 6);
 
             x = canvas.centerX();
             y = canvas.centerY();
@@ -80,46 +86,51 @@ private:
         }
 
     private:
-        bool isOutOfCanvasX(const kf::gfx::Canvas &canvas) const {
+        [[nodiscard]] bool isOutOfCanvasX(const Canvas &canvas) const {
             return x < radius or x + radius > canvas.maxX();
         }
 
-        bool isOutOfCanvasY(const kf::gfx::Canvas &canvas) const {
+        [[nodiscard]] bool isOutOfCanvasY(const Canvas &canvas) const {
             return y < radius or y + radius > canvas.maxY();
         }
 
-    } ball{};
+    };
 
-    kf::gfx::Canvas canvas;
+    Wall left_wall{};
+    Wall right_wall{};
+    Ball ball{};
+
+    Canvas scene_canvas;
 
 public:
     void onEntry() override {
-        ball.reset(canvas);
-        left_wall.reset(canvas, Wall::Side::Left);
-        right_wall.reset(canvas, Wall::Side::Right);
+        ball.reset(scene_canvas);
+        left_wall.reset(scene_canvas, Wall::Side::Left);
+        right_wall.reset(scene_canvas, Wall::Side::Right);
     }
 
-    void updateLayout(kf::gfx::Canvas &root) override {
-        auto [up, down] = root.splitVertically<2>({1, 7});
-
-        canvas = down;
+    void updateLayout(Canvas &root) override {
+        scene_canvas = root;
     }
 
     void display() override {
         Behavior::display();
-        
-        canvas.line(0, 0, canvas.maxX(), 0);
-        canvas.line(0, canvas.maxY(), canvas.maxX(), canvas.maxY());
 
-        ball.render(canvas);
-        left_wall.render(canvas);
-        right_wall.render(canvas);
+        scene_canvas.line(0, 0, scene_canvas.maxX(), 0);
+        scene_canvas.line(0, scene_canvas.maxY(), scene_canvas.maxX(), scene_canvas.maxY());
+
+        ball.render(scene_canvas);
+        left_wall.render(scene_canvas);
+        right_wall.render(scene_canvas);
     }
 
     void update() override {
-        ball.update(canvas);
-        left_wall.update(canvas);
-        right_wall.update(canvas);
+        auto &periphery = Periphery::instance();
+
+        ball.update(scene_canvas);
+
+        left_wall.update(periphery.left_joystick.axis_y.read());
+        right_wall.update(periphery.right_joystick.axis_y.read());
     }
 };
 
