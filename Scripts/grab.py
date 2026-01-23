ace38393e219395f8b2c0aa8c068b4ffff528055
KiraFlux <pret2titio1n@gmail.com>
2026-01-23 18:21:37 +0300

    add Scripts

---
+++ Scripts/grab.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+DIR = Path("../DJC-Firmware/src")
+
+for file in DIR.rglob("*.hpp"):
+    try:
+        print(f"{file.relative_to(DIR)}")
+        print("```cpp")
+        print(file.read_text(encoding='utf-8', errors='ignore').rstrip())
+        print("```\n")
+    except:
+        pass
+
+for file in DIR.rglob("*.cpp"):
+    try:
+        print(f"{file.relative_to(DIR)}")
+        print("```cpp")
+        print(file.read_text(encoding='utf-8', errors='ignore').rstrip())
+        print("```\n")
+    except:
+        pass
