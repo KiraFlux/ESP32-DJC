3690595e5a66c07675ff24fd85c1485376e85e63
KiraFlux <pret2titio1n@gmail.com>
2026-04-02 15:30:10 +0300

    da

--- DJC-Firmware/src/djc/Application.hpp
+++ DJC-Firmware/src/djc/Application.hpp
@@ -3,8 +3,32 @@
 
 #pragma once
 
+#include <kf/math/units.hpp>
+#include <kf/mixin/Initable.hpp>
+#include <kf/mixin/NonCopyable.hpp>
+#include <kf/mixin/TimedPollable.hpp>
+
+#include "djc/prelude.hpp"
+#include "djc/Periphery.hpp"
+
 namespace djc {
 
-struct Application {};
+struct Application final : kf::mixin::Initable<Application, bool>, kf::mixin::NonCopyable, kf::mixin::TimedPollable<Application> {
+
+private:
+
+
+
+    // impl
+
+    KF_IMPL_INITABLE(Application, bool);
+    bool initImpl() noexcept {
+        return true;
+    }
+
+    KF_IMPL_TIMED_POLLABLE(Application);
+    void pollImpl(kf::math::Milliseconds now) noexcept {
+    }
+};
 
 }// namespace djc
