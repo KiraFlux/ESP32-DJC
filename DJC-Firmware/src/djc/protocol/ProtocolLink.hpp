898bbd036f163c8bf17fcdc59318feaacd95fc20
KiraFlux <pret2titio1n@gmail.com>
2026-05-04 11:26:38 +0300

    update: ProtocolLink

--- DJC-Firmware/src/djc/protocol/ProtocolLink.hpp
+++ DJC-Firmware/src/djc/protocol/ProtocolLink.hpp
@@ -6,8 +6,10 @@
 #include <kf/Logger.hpp>
 
 #include <kf/aliases.hpp>
+#include <kf/math/Timer.hpp>
 #include <kf/math/units.hpp>
 #include <kf/memory/Slice.hpp>
+#include <kf/mixin/Configurable.hpp>
 #include <kf/mixin/NonCopyable.hpp>
 
 #include "djc/ManualInput.hpp"
@@ -16,7 +18,30 @@
 
 namespace djc::protocol {
 
-struct ProtocolLink : kf::mixin::NonCopyable {
+namespace internal {
+
+struct ProtocolLinkConfig {
+
+    kf::math::Milliseconds poll_period;
+
+    [[nodiscard]] static constexpr ProtocolLinkConfig defaults() noexcept {
+        return ProtocolLinkConfig{
+            .poll_period = static_cast<kf::math::Milliseconds>(1000 / 50),
+        };
+    }
+};
+
+}// namespace internal
+
+struct ProtocolLink :
+
+    kf::mixin::NonCopyable,
+    kf::mixin::Configurable<internal::ProtocolLinkConfig>
+
+{
+    using Config = internal::ProtocolLinkConfig;
+
+    using kf::mixin::Configurable<Config>::Configurable;
 
     void protocol(Protocol &new_protocol) noexcept {
         _protocol = &new_protocol;
@@ -28,7 +53,12 @@ struct ProtocolLink : kf::mixin::NonCopyable {
             return;
         }
 
-        _protocol->poll(now, input, transport_link);
+        if (_poll_timer.expired(now) or _poll_timer_reset_required) {
+            _poll_timer.start(now);
+            _poll_timer_reset_required = false;
+
+            _protocol->poll(now, input, transport_link);
+        }
     }
 
     void receive(kf::memory::Slice<const kf::u8> buffer) noexcept {
@@ -44,6 +74,8 @@ private:
     static constexpr auto logger{kf::Logger::create("ProtocolLink")};
 
     Protocol *_protocol{nullptr};
+    kf::math::Timer _poll_timer{this->config().poll_period};
+    bool _poll_timer_reset_required{true};
 };
 
 }// namespace djc::protocol
