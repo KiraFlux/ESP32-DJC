26bf66725a8f80f6b7aca3c0daa29cf0fc5efb3d
KiraFlux <pret2titio1n@gmail.com>
2026-06-14 20:10:16 +0300

    feat(system): add config system

---
+++ DJC-Firmware/src/djc/system/ConfigSystem.hpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2026 KiraFlux
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include "djc/service/ConfigService.hpp"
+#include "djc/system/System.hpp"
+
+namespace djc::system {
+
+/// @brief System owning the config service, handling deferred NVS operations
+/// @note Wraps ConfigService, requests load on init, and polls it periodically
+struct ConfigSystem : System<ConfigSystem> {
+private:
+    service::ConfigService _config_service{};
+
+    KF_IMPL_INITABLE(ConfigSystem, bool);
+    bool initImpl() noexcept {
+        _config_service.requestLoad();
+
+        return true;
+    }
+
+    KF_IMPL_TIMED_POLLABLE(ConfigSystem);
+    void poll(kf::math::Milliseconds now) noexcept {
+        _config_service.poll(now);
+    }
+};
+
+}// namespace djc::system
