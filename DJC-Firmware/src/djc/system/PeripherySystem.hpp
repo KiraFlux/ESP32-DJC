d063eb98af636b35c64ab92026f0fc7e9c56ef87
KiraFlux <pret2titio1n@gmail.com>
2026-06-14 23:45:58 +0300

    refactor(system): remove ConfigService dependency from PeripherySystem

--- DJC-Firmware/src/djc/system/PeripherySystem.hpp
+++ DJC-Firmware/src/djc/system/PeripherySystem.hpp
@@ -4,19 +4,19 @@
 #pragma once
 
 #include <kf/Logger.hpp>
+#include <kf/mixin/Configurable.hpp>
 
+#include "djc/Config.hpp"
 #include "djc/Periphery.hpp"
-#include "djc/service/ConfigService.hpp"
 #include "djc/system/System.hpp"
 
 namespace djc::system {
 
 /// @brief System that owns and initializes hardware peripherals
 /// @note Provides access to peripherals for other systems via getters; no periodic polling needed.
-struct PeripherySystem : System<PeripherySystem> {
+struct PeripherySystem : System<PeripherySystem>, kf::mixin::Configurable<Config> {
 
-    explicit PeripherySystem(service::ConfigService &config_service) noexcept :
-        _config_service{config_service} {}
+    using kf::mixin::Configurable<Config>::Configurable;
 
     /// @brief Get mutable access to periphery component
     Periphery &periphery() noexcept {
@@ -28,29 +28,29 @@ struct PeripherySystem : System<PeripherySystem> {
         return _periphery;
     }
 
+    /// @brief Tune periphery
+    void tune(Periphery::Config &mutable_periphery_config) noexcept {
+        if (mutable_periphery_config.joystick_axes_tuned) {
+            logger.debug("Nothing to do: axes already tuned");
+        } else {
+            logger.debug("Tunning axes..");
+            _periphery.tune(mutable_periphery_config);
+        }
+    }
+
 private:
     static constexpr auto logger{kf::Logger::create("PeripherySystem")};
 
-    service::ConfigService &_config_service;
-    Periphery _periphery{_config_service.config().periphery};
+    Periphery _periphery{this->config().periphery};
 
     KF_IMPL_INITABLE(PeripherySystem, bool);
     bool initImpl() noexcept {
-        if (not _periphery.init()) {
-            logger.error("Periphery init failed. Resseting periphery config to defaults");
-            _config_service.config().periphery = djc::Periphery::Config::defaults();
-            _config_service.requestSave();
-
+        if (_periphery.init()) {
+            return true;
+        } else {
+            logger.error("Periphery init failed");
             return false;
         }
-
-        if (not _config_service.config().periphery.joystick_axes_tuned) {
-            logger.debug("Tunning axes..");
-            _periphery.tune(_config_service.config().periphery);
-            _config_service.requestSave();
-        }
-
-        return true;
     }
 
     KF_IMPL_TIMED_POLLABLE(PeripherySystem);
