a661d97a68819868687c6c20d77aae305195488e
KiraFlux <pret2titio1n@gmail.com>
2026-06-15 01:18:52 +0300

    feat(system): add ui system

---
+++ DJC-Firmware/src/djc/system/UiSystem.hpp
@@ -0,0 +1,87 @@
+// Copyright (c) 2026 KiraFlux
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <kf/Logger.hpp>
+
+#include "djc/Config.hpp"
+#include "djc/system/System.hpp"
+#include "djc/ui/UI.hpp"
+#include "djc/ui/VirtualKeyboard.hpp"
+#include "djc/ui/pages/RootPage.hpp"
+
+namespace djc::system {
+
+/// @brief System that owns UI components: renderer, virtual keyboard, UI instance and root page
+/// @note Initialises the UI with the root page and triggers an initial update event. Polls the UI on every main loop iteration
+struct UiSystem : System<UiSystem> {
+
+    using Renderer = ui::UI::Traits::RenderImpl;
+
+    explicit UiSystem(const Config &config) noexcept :
+        _renderer{config.render_system} {}
+
+    /// @brief Get mutable access to renderer component
+    Renderer &renderer() noexcept {
+        return _renderer;
+    }
+
+    /// @brief Get readonly access to renderer component
+    constexpr const Renderer &renderer() const noexcept {
+        return _renderer;
+    }
+
+    /// @brief Get mutable access to virtual keyboard component
+    ui::VirtualKeyboard &virtualKeyboard() noexcept {
+        return _virtual_keyboard;
+    }
+
+    /// @brief Get readonly access to virtual keyboard component
+    constexpr const ui::VirtualKeyboard &virtualKeyboard() const noexcept {
+        return _virtual_keyboard;
+    }
+
+    /// @brief Get mutable access to ui service
+    ui::UI &ui() noexcept {
+        return _ui_service;
+    }
+
+    /// @brief Get readonly access to ui service
+    constexpr const ui::UI &ui() const noexcept {
+        return _ui_service;
+    }
+
+    /// @brief Get mutable access to ui service
+    ui::pages::RootPage &rootPage() noexcept {
+        return _root_page;
+    }
+
+    /// @brief Get readonly access to ui service
+    constexpr const ui::pages::RootPage &rootPage() const noexcept {
+        return _root_page;
+    }
+
+private:
+    static constexpr auto logger{kf::Logger::create("UiSystem")};
+
+    Renderer _renderer;
+    ui::VirtualKeyboard _virtual_keyboard{};
+    ui::UI _ui_service{_renderer, _virtual_keyboard};
+    ui::pages::RootPage _root_page{_ui_service};
+
+    KF_IMPL_INITABLE(UiSystem, bool);
+    bool initImpl() noexcept {
+        _ui_service.activePage(_root_page);
+        _ui_service.addEvent(ui::UI::Traits::EventImpl::update());
+
+        return true;
+    }
+
+    KF_IMPL_TIMED_POLLABLE(UiSystem);
+    void poll(kf::math::Milliseconds now) noexcept {
+        _ui_service.poll(now);
+    }
+};
+
+}// namespace djc::system
