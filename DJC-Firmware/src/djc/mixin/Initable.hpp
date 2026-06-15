909f4fcef1d7e8df2718a23a27af49efab7162ec
KiraFlux <pret2titio1n@gmail.com>
2026-06-15 14:03:52 +0300

    feat(mixin): add initable mixin with init arguments

---
+++ DJC-Firmware/src/djc/mixin/Initable.hpp
@@ -0,0 +1,35 @@
+// Copyright (c) 2026 KiraFlux
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <utility>
+
+namespace djc::mixin {
+
+/// @brief CRTP mixin that adds an `init(Args...)` method forwarding arguments to `initImpl(Args...)`
+/// @tparam Impl     Derived class (must implement `initImpl(Args...)`)
+template<typename Impl, typename Signature> struct Initable;
+
+template<typename Impl, typename R, typename... Args> struct Initable<Impl, R(Args...)> {
+
+    /// @brief Initializes the object with given arguments
+    /// @param args Arguments forwarded to `initImpl`
+    /// @return init result
+    [[nodiscard]] R init(Args... args) noexcept {
+        return static_cast<Impl *>(this)->initImpl(std::forward<Args>(args)...);
+    }
+};
+
+template<typename Impl, typename... Args> struct Initable<Impl, void(Args...)> {
+
+    /// @brief Initializes the object with given arguments
+    /// @param args Arguments forwarded to `initImpl`
+    void init(Args... args) noexcept {
+        static_cast<Impl *>(this)->initImpl(std::forward<Args>(args)...);
+    }
+};
+
+#define DJC_IMPL_INITABLE(__impl__, ...) friend struct ::djc::mixin::Initable<__impl__, __VA_ARGS__>
+
+}// namespace djc::mixin
