78662b11e998897877f46adc2c6b4f136f3515f8
KiraFlux <pret2titio1n@gmail.com>
2026-06-15 13:39:45 +0300

    refactor(system/input): use service owner mixin

--- DJC-Firmware/src/djc/system/InputSystem.hpp
+++ DJC-Firmware/src/djc/system/InputSystem.hpp
@@ -3,10 +3,9 @@
 
 #pragma once
 
-#include <kf/mixin/Configurable.hpp>
-
 #include "djc/Config.hpp"
 #include "djc/Periphery.hpp"
+#include "djc/mixin/ServiceOwner.hpp"
 #include "djc/service/InputHandler.hpp"
 #include "djc/system/System.hpp"
 
@@ -14,30 +13,21 @@ namespace djc::system {
 
 /// @brief System managing user input
 /// @note Owns InputHandler service. Depends on Config (read‑only) and Periphery.
-struct InputSystem : System<InputSystem>, kf::mixin::Configurable<Config> {
+struct InputSystem :
 
+    System<InputSystem>,
+    mixin::ServiceOwner<service::InputHandler>
+
+{
     explicit InputSystem(const Config &config, Periphery &periphery) noexcept :
-        kf::mixin::Configurable<Config>{config},
-        _input_handler{
+        mixin::ServiceOwner<service::InputHandler>{service::InputHandler{
             config.input_handler,
             periphery.right_joystick,// primary joystick for navigation/control
             periphery.left_button_listener,
             periphery.right_button_listener,
-        } {}
-
-    /// @brief Get mutable access to the input handler service
-    service::InputHandler &service() noexcept {
-        return _input_handler;
-    }
-
-    /// @brief Get readonly access to the input handler service
-    constexpr const service::InputHandler &service() const noexcept {
-        return _input_handler;
-    }
+        }} {}
 
 private:
-    service::InputHandler _input_handler;
-
     KF_IMPL_INITABLE(InputSystem, bool);
     bool initImpl() noexcept {
         // For now, nothing to do.
@@ -46,7 +36,7 @@ private:
 
     KF_IMPL_TIMED_POLLABLE(InputSystem);
     void pollImpl(kf::math::Milliseconds now) noexcept {
-        _input_handler.poll(now);
+        this->service().poll(now);
     }
 };
 
