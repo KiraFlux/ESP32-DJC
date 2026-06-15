6b274a7aa36a16b6d1c9afa5da1114d509a5c133
KiraFlux <pret2titio1n@gmail.com>
2026-06-15 14:55:44 +0300

    refactor(system/ui): add pages list as init argument

--- DJC-Firmware/src/djc/system/UiSystem.hpp
+++ DJC-Firmware/src/djc/system/UiSystem.hpp
@@ -3,10 +3,12 @@
 
 #pragma once
 
+#include <initializer_list>
+
 #include <kf/Logger.hpp>
 
-#include "djc/mixin/ServiceOwner.hpp"
 #include "djc/Config.hpp"
+#include "djc/mixin/ServiceOwner.hpp"
 #include "djc/system/System.hpp"
 #include "djc/ui/UI.hpp"
 #include "djc/ui/VirtualKeyboard.hpp"
@@ -16,16 +18,16 @@ namespace djc::system {
 
 /// @brief System that owns UI components: renderer, virtual keyboard, UI instance and root page
 /// @note Initialises the UI with the root page and triggers an initial update event. Polls the UI on every main loop iteration
-struct UiSystem : 
+struct UiSystem :
 
-System<UiSystem, void()>,
-mixin::ServiceOwner<djc::ui::UI>
+    System<UiSystem, void(std::initializer_list<ui::UI::Page *>)>,
+    mixin::ServiceOwner<djc::ui::UI>
 
 {
     using Renderer = ui::UI::Traits::RenderImpl;
 
     explicit UiSystem(const Config &config) noexcept :
-        mixin::ServiceOwner<djc::ui::UI>{djc::ui::UI{_renderer, _virtual_keyboard}}, 
+        mixin::ServiceOwner<djc::ui::UI>{djc::ui::UI{_renderer, _virtual_keyboard}},
         _renderer{config.render_system} {}
 
     /// @brief Get mutable access to renderer component
@@ -65,8 +67,12 @@ private:
     ui::VirtualKeyboard _virtual_keyboard{};
     ui::pages::RootPage _root_page{this->service()};
 
-    DJC_IMPL_INITABLE(UiSystem, void());
-    void initImpl() noexcept {
+    DJC_IMPL_INITABLE(UiSystem, void(std::initializer_list<ui::UI::Page *>));
+    void initImpl(std::initializer_list<ui::UI::Page *> pages) noexcept {
+        for (auto page : pages) {
+            _root_page.attach(*page);
+        }
+        
         this->service().activePage(_root_page);
         this->service().addEvent(ui::UI::Traits::EventImpl::update());
     }
