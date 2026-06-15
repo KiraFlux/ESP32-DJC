912cc6ff90c40ecdf1bd0dd6db78c65ffd6166e9
KiraFlux <pret2titio1n@gmail.com>
2026-06-15 13:35:15 +0300

    refactor(system/graphics): use service owner mixin

--- DJC-Firmware/src/djc/system/GraphicsSystem.hpp
+++ DJC-Firmware/src/djc/system/GraphicsSystem.hpp
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include "djc/mixin/ServiceOwner.hpp"
 #include "djc/service/DisplayManager.hpp"
 #include "djc/system/System.hpp"
 #include "djc/ui/VirtualKeyboard.hpp"
@@ -10,25 +11,18 @@
 namespace djc::system {
 
 /// @brief System managing display output, canvas, and virtual keyboard.
-template<typename I> struct GraphicsSystem : System<GraphicsSystem<I>> {
+template<typename I> struct GraphicsSystem :
+
+    System<GraphicsSystem<I>>,
+    mixin::ServiceOwner<service::DisplayManager<I>>
+
+{
     using DisplayManagerImpl = service::DisplayManager<I>;
 
     explicit GraphicsSystem(I &display_driver, const ui::VirtualKeyboard &virtual_keyboard) noexcept :
-        _display_manager{_display_driver, virtual_keyboard} {}
-
-    /// @brief Get mutable access to display service
-    DisplayManagerImpl &service() noexcept {
-        return _display_manager;
-    }
-
-    /// @brief Get readonly access to display service
-    constexpr const DisplayManagerImpl &service() const noexcept {
-        return _display_manager;
-    }
+        mixin::ServiceOwner<DisplayManagerImpl>{DisplayManagerImpl{_display_driver, virtual_keyboard}} {}
 
 private:
-    DisplayManagerImpl _display_manager;
-
     using This = GraphicsSystem<I>;
 
     KF_IMPL_INITABLE(This, bool);
